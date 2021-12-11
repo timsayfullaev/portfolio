2e57b20a876eff61e03853af41a6e1d6eb7061f9
Timur <tim.sayfullaev@gmail.com>
2021-12-11 18:13:42 +0500

    about

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,8 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       menuClose = document.querySelector('.menu__close'),
-      overlay = document.querySelector('.menu__overlay');
+      overlay = document.querySelector('.menu__overlay'),
+      menuLink = document.querySelectorAll('.menu__link');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('menu_active');
@@ -13,4 +14,10 @@ menuClose.addEventListener('click', () => {
 
 overlay.addEventListener('click', () => {
     menu.classList.remove('menu_active');
+});
+
+menuLink.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('menu_active');
+    });
 });
