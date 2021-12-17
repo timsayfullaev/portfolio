c0e98c6ff69286ad25ea1175d5a1e35c3c891883
Timur <tim.sayfullaev@gmail.com>
2021-12-17 09:50:54 +0500

    skills

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,8 @@
 const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu'),
-      menuClose = document.querySelector('.menu__close'),
-      overlay = document.querySelector('.menu__overlay'),
-      menuLink = document.querySelectorAll('.menu__link');
+    menu = document.querySelector('.menu'),
+    menuClose = document.querySelector('.menu__close'),
+    overlay = document.querySelector('.menu__overlay'),
+    menuLink = document.querySelectorAll('.menu__link');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('menu_active');
@@ -20,4 +20,11 @@ menuLink.forEach(link => {
     link.addEventListener('click', () => {
         menu.classList.remove('menu_active');
     });
+});
+
+const counters = document.querySelectorAll('.skills__percentages-percent'),
+    lines = document.querySelectorAll('.skills__percentages-progress');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
 });
